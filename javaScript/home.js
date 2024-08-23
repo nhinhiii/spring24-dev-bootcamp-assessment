fbbfb7103e346cc4abe1085782b00af69f98f7f5
nhinhiii <chaochochochao@gmail.com>
2024-08-23 17:50:06 -0400

    add icon close and open, create JS for these icon

--- javaScript/home.js
+++ javaScript/home.js
@@ -1,2 +1,12 @@
 const year = new Date().getFullYear();
 document.getElementById('year').textContent = year;
+
+var navLinks = document.getElementById("navLinks");
+
+function open(){
+    navLinks.style.right = "0";
+}
+
+function close(){
+    navLinks.style.right = "-200px";
+}
